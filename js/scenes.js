ea04cab09bf31ab427d19f46f99c03b88d0131b9
Ethan7425 <112752276+Ethan7425@users.noreply.github.com>
2024-01-20 23:39:05 +0100

    color theory start

---
+++ js/scenes.js
@@ -0,0 +1,29 @@
+function changeScene(sceneId) {
+	// Get all buttons
+	const buttons = document.querySelectorAll('.navButton');
+  
+	// Remove active class from all buttons
+	buttons.forEach(button => {
+	  button.classList.remove('active');
+	});
+  
+	// Get clicked button and add active class
+	const clickedButton = document.getElementById(sceneId + 'Button');
+	clickedButton.classList.add('active');
+  
+	// Hide all scenes
+	const scenes = document.querySelectorAll('.scene');
+	scenes.forEach(scene => {
+	  scene.style.display = 'none';
+	});
+  
+	// Show clicked scene
+	const desiredScene = document.getElementById(sceneId);
+	desiredScene.style.display = 'block';
+  }
+  
+  // Event listeners for navbar buttons
+  document.getElementById("homeButton").addEventListener("click", () => changeScene('home'));
+  document.getElementById("profileButton").addEventListener("click", () => changeScene('profile'));
+  document.getElementById("settingsButton").addEventListener("click", () => changeScene('settings'));
+  
